8f0e820e360f948eac51eda83e0cadaa38492f0c
LiHaoHN <1309731011@qq.com>
2025-02-14 16:06:19 +0800

    重新添加所有文件

--- scripts_test_SimplerEnv/double_run_15.sh
+++
@@ -1,5 +0,0 @@
-bash /mnt/petrelfs/lihao3/EmbodiedMLLM/CogACTx_DIT_Atten_HisF_MultiF_R_Silence/scripts_test_SimplerEnv/RUN_all_test_cogact_24.sh &
-pid1=$!
-bash /mnt/petrelfs/lihao3/EmbodiedMLLM/CogACTx_DIT_Atten_HisF_MultiF_R_Silence/scripts_test_SimplerEnv/RUN_all_test_cogact_25.sh &
-pid2=$!
-wait $pid1 $pid2
--- scripts_test_SimplerEnv/double_run_4.sh
+++
@@ -1,5 +0,0 @@
-bash /mnt/petrelfs/lihao3/EmbodiedMLLM/CogACTx_DIT_Atten_HisF_MultiF_R_Silence/scripts_test_SimplerEnv/RUN_all_test_cogact_7.sh &
-pid1=$!
-bash /mnt/petrelfs/lihao3/EmbodiedMLLM/CogACTx_DIT_Atten_HisF_MultiF_R_Silence/scripts_test_SimplerEnv/RUN_all_test_cogact_8.sh &
-pid2=$!
-wait $pid1 $pid2
